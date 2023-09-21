bc5b9cc4e8be60c9ed1499cf4563f6c2d5e4f9d1
GhouI <abdulalian201@gmail.com>
2023-09-21 14:31:07 +0100

    Better System
    Allowed better system blow with cards and project cards.

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
 import React from 'react'
-import DiscordThumb from "@/public/discord-thumb.webp"
-import AnotherThumb from "@/public/discord-thumb.webp"
 import ProjectCard from './ProjectCard'
+import Cards from "@/lib/Cards"
 const Projects = () => {
   return (
 <div className='bg-gray-50 h-full py-5'>
@@ -12,40 +10,21 @@ const Projects = () => {
       Here you will find some of my past projects, both personal and professional, that I've worked on over the years.
     </p>
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-      {/* Project 1 */}
-      <ProjectCard
-        title="Discord Application"
-        description="This is a Discord application where you can invite it to your server and play various types of games. This bot is primarily focused on role-playing game elements, such as leveling up, gaining experience, and fighting monsters. It has been completed and used by over 1,000 users."
-        technologies={["Node.js", "MongoDB", "AWS"]}
-        imageSrc={DiscordThumb}
-        altText="Discord Application Logo"
-        liveDemoLink="#"
-        sourceCodeLink="#"
-        heightOfImage={1200}
-        widthOfImage={1200}
-      />
-            <ProjectCard
-        title="Discord Application"
-        description="This is a Discord application where you can invite it to your server and play various types of games. This bot is primarily focused on role-playing game elements, such as leveling up, gaining experience, and fighting monsters. It has been completed and used by over 1,000 users."
-        technologies={["Node.js", "MongoDB", "AWS"]}
-        imageSrc={DiscordThumb}
-        altText="Discord Application Logo"
-        liveDemoLink="#"
-        sourceCodeLink="#"
-        heightOfImage={1200}
-        widthOfImage={1200}
-      />
-            <ProjectCard
-        title="Discord Application"
-        description="This is a Discord application where you can invite it to your server and play various types of games. This bot is primarily focused on role-playing game elements, such as leveling up, gaining experience, and fighting monsters. It has been completed and used by over 1,000 users."
-        technologies={["Node.js", "MongoDB", "AWS"]}
-        imageSrc={DiscordThumb}
-        altText="Discord Application Logo"
-        liveDemoLink="#"
-        sourceCodeLink="#"
-        heightOfImage={1200}
-        widthOfImage={1200}
-      />
+      {/* Projects */}
+      {Cards.map((card, index) => (
+        <ProjectCard
+          key={index}
+          title={card.title}
+          description={card.description}
+          technologies={card.technologies}
+          imageSrc={card.imageSrc}
+          altText={card.altText}
+          liveDemoLink={card.liveDemoLink}
+          sourceCodeLink={card.sourceCodeLink}
+          heightOfImage={card.heightOfImage}
+          widthOfImage={card.widthOfImage}
+        />
+      ))}
     </div>
   </div>
 </div>
